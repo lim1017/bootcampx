c3e054213697b3ec9eec52a6abc2eb2e0328a6b2
lim1017 <lim1017@hotmail.com>
2020-02-03 20:47:00 +0000

    changed incorrect file name

--- migrations/assignment_submissions.sql
+++ migrations/assignment_submissions.sql
@@ -1,5 +1,16 @@
-SELECT sum(assignment_submissions.duration) as total_duration
-FROM assignment_submissions
-JOIN students ON students.id = student_id
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = 'FEB12';
+CREATE TABLE assignments (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255),
+  content TEXT,
+  day INTEGER,
+  chapter INTEGER,
+  duration INTEGER
+);
+
+CREATE TABLE assignment_submissions (
+  id SERIAL PRIMARY KEY NOT NULL,
+  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+  student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
+  duration INTEGER,
+  submission_date DATE
+);
