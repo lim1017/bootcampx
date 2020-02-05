cafd3a725277ca89281e7b09b3ddfbdb7c87d457
lim1017 <lim1017@hotmail.com>
2020-02-05 21:17:41 +0000

    used postgre /node to run sql queries

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,31 @@
+let myArgs = process.argv.slice(2);
+
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
+SELECT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests) as total_assistances
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE '%${myArgs[0]}%'
+GROUP BY teachers.name, cohorts.name
+ORDER BY teacher;
+`)
+.then(res => {
+
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
+  })
+
+
+})
+.catch(err => console.error('query error', err.stack));
