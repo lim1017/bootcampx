ce212996fe92b0d8f3e1545febc906c1fdf0c2dc
lim1017 <lim1017@hotmail.com>
2020-02-05 21:51:42 +0000

    protected against sql injections

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,6 +1,12 @@
 let myArgs = process.argv.slice(2);
 
-console.log(myArgs)
+let cohortName=myArgs[0]
+let limit=myArgs[1]
+
+console.log(cohortName)
+console.log(limit)
+
+const values = [`%${cohortName}%`, limit];
 
 
 const { Pool } = require('pg');
@@ -17,11 +23,12 @@ pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohortz
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${myArgs[0]}%'
-LIMIT ${myArgs[1]};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohortz} cohort`);
   })
-});
+})
+.catch(err => console.error('query error', err.stack));
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,8 +1,10 @@
 let myArgs = process.argv.slice(2);
 
+let cohortName=myArgs[0]
+
+
 
 const { Pool } = require('pg');
-
 const pool = new Pool({
   user: 'vagrant',
   password: '123',
@@ -10,16 +12,18 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const values = [`%${cohortName}%`];
+
 pool.query(`
 SELECT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests) as total_assistances
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${myArgs[0]}%'
+WHERE cohorts.name LIKE $1
 GROUP BY teachers.name, cohorts.name
 ORDER BY teacher;
-`)
+`, values)
 .then(res => {
 
   res.rows.forEach(row => {
